1f5f7aabccce1a2a8505d5e4398815ba42390f51
Nathan Hawkins <Nathan.Hawkins@YRCFreight.com>
2018-05-15 12:55:03 -0500

    initial gh-pages deploy

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyCHVsWgPi_6arItPfwneWCGKjQmqum6KIs',
-    authDomain: 'paradise-archery-schedule.firebaseapp.com',
-    databaseURL: 'https://paradise-archery-schedule.firebaseio.com',
-    storageBucket: 'paradise-archery-schedule.appspot.com'
-  }
-};
