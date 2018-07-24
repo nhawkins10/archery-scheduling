6c14b6acc80cf4f108cb72aef3b9f45f13cb83be
Nathan Hawkins <Nathan.Hawkins@YRCFreight.com>
2018-07-24 14:23:18 -0500

    adding phone number field

--- src/app/roster-edit/roster-edit.component.ts
+++ src/app/roster-edit/roster-edit.component.ts
@@ -9,6 +9,7 @@ export class RosterEdit {
   @Output() onAddVolunteer = new EventEmitter<String>();
   @Output() onHideEdit = new EventEmitter<String>();
   name = '';
+  phone = '';
   when = 'BEFORE';
   role1 = 'RANGE';
   role2 = 'DESK';
@@ -20,12 +21,14 @@ export class RosterEdit {
     }
     var person = {
       'name': this.name,
+      'phone': this.phone,
       'when': this.when,
       'role1': this.role1,
       'role2': this.role2,
       'selected': false
     }
     window.localStorage.setItem('name', this.name);
+    window.localStorage.setItem('phone', this.phone);
     this.onAddVolunteer.emit(JSON.stringify(person));
     this.onHideEdit.emit();
   }
@@ -37,6 +40,10 @@ export class RosterEdit {
   ngOnInit(): void {
     if (localStorage.getItem('name')) {
       this.name = localStorage.getItem('name');
+    }
+
+    if (localStorage.getItem('phone')) {
+      this.phone = localStorage.getItem('phone');
     }
   }
 }
